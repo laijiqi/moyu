84c5aab808e9d9385aff155bcc3aba22a67c14fa
laijiqi <66714803+laijiqi@users.noreply.github.com>
2020-06-10 13:53:11 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -1,1 +1,1 @@
-url = 'https://oapi.dingtalk.com/robot/send?access_token=dfb282779fa574887abaade18616db9be415fcc4b11289a6bd487a59f2b5a641'
+url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d407da4e-63da-4b49-a07a-4364c57140db'
